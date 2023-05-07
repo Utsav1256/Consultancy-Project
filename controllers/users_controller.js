fa6f61f9ee90af335737b12c1b7312a0a8e7476f
utsav kumar <utsavkumar5462@gmail.com>
2023-05-07 15:28:17 +0530

    Complete authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -52,3 +52,13 @@ module.exports.create = function (req, res) {
 module.exports.createSession = function (req, res) {
   return res.redirect("/");
 };
+
+// sign out
+module.exports.destroySession = function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    return res.redirect("/");
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -17,4 +17,6 @@ router.post(
   userController.createSession
 );
 
+router.get("/sign-out", userController.destroySession);
+
 module.exports = router;
