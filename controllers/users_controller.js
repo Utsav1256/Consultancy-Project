0da095b2c79de7b9a106014884cf0e673f9d32d3
utsav kumar <utsavkumar5462@gmail.com>
2023-07-20 10:33:05 +0530

    setup Profile page update feature

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -24,10 +24,19 @@ module.exports.profile = function (req, res) {
 };
 module.exports.update = function (req, res) {
   if (req.user.id == req.params.id) {
-    User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
-      //  req.body -> {name: req.body.name, email: req,body.email}
-      return res.redirect("back");
-    });
+    // User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+    //   //  req.body -> {name: req.body.name, email: req,body.email}
+    //   return res.redirect("back");
+    // });
+    User.findByIdAndUpdate(req.params.id, req.body)
+      .then((user) => {
+        // req.body -> {name: req.body.name, email: req.body.email}
+        return res.redirect("back");
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+      });
   } else {
     return res.status(401).send("Unauthorized");
   }
@@ -43,22 +52,29 @@ module.exports.signUp = function (req, res) {
   });
 };
 
-// render the payment page
+// render the register page
 module.exports.register = function (req, res) {
-  if (req.isAuthenticated()) {
-    // return res.redirect("/users/register");
-    return res.render("user_register", {
-      title: "Registration",
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res.render("user_register", {
+          title: "Registration",
+          profile_user: user,
+        });
+      } else {
+        return res.render("user_sign_up", {
+          title: "Sign Up",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  }
-  return res.render("user_sign_up", {
-    title: "Sign Up",
-  });
 };
 // render the sign in page
 module.exports.signIn = function (req, res) {
   if (req.isAuthenticated()) {
-    return res.redirect("/users/profile");
+    return res.redirect("/users/profile/:id");
   }
   return res.render("user_sign_in", {
     title: "Sign_In",
