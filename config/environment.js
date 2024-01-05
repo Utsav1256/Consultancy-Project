d128da88885ee8572d420110c21c674365d9296c
utsav kumar <utsavkumar5462@gmail.com>
2024-01-06 03:06:31 +0530

    setup environt variable for production

--- config/environment.js
+++ config/environment.js
@@ -20,5 +20,24 @@ const development = {
 };
 const production = {
   name: "production",
+  asset_path: process.env.CONSULTANCY_ASSET_PATH,
+  session_cookie_key: process.env.CONSULTANCY_SESSION_COOKIE_KEY,
+  db: "consultancy_development",
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.CONSULTANCY_GMAIL_USERNAME,
+      pass: process.env.CONSULTANCY_GMAIL_PASSWORD,
+    },
+  },
+  google_client_id: process.env.CONSULTANCY_GOOGLE_CLIENT_ID,
+  google_client_secret: process.env.CONSULTANCY_GOOGLE_CLIENT_SECRET,
+  google_call_back_url: process.env.CONSULTANCY_GOOGLE_CALLBACK_URL,
 };
-module.exports = development;
+module.exports =
+  eval(process.env.CONSULTANCY_ENVIRONMENT) == undefined
+    ? development
+    : eval(process.env.CONSULTANCY_ENVIRONMENT);
